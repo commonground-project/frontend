75d751b40f38cc83f2d18bbc6c6f03808db5b92b
Mrbandi <qwea6935@gmail.com>
2024-11-20 20:32:00 +0800

    feat: Implement dynamic API redirect configuration based on environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async redirects() {
-        return [
+        let apiDomain;
+        const environment = process.env.NEXT_PUBLIC_VERCEL_ENV || 'development';
+
+        switch (environment) {
+            case 'perview':
+                apiDomain = 'https://api.dev.commonground.tw';
+                break;
+            case 'production':
+                apiDomain = 'https://api.stage.commonground.tw';
+                break;
+            default:
+                apiDomain = 'http://localhost:3000';
+                break;
+        }
+
+        if (environment === 'perview') {
+            return [
             {
                 source: '/api/:path*',
-                destination: 'https://api.commonground.tw/callback?:path*',
-                permanent: true,
+                destination: `${apiDomain}/api/:path*`,
+                permanent: false,
             },
-        ];
+            {
+                source: '/login',
+                destination: `${apiDomain}/login`,
+                permanent: false,
+            },
+            ];
+        } else if (environment === 'production') {
+            return [
+            {
+                source: '/api/:path*',
+                destination: `${apiDomain}/api/:path*`,
+                permanent: false,
+            },
+            ];
+        } else {
+            return [];
+        }
     },
 };
 
