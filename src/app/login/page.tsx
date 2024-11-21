4ff17d28a831b76e16ae55d1a1d7fc6367a451eb
Mrbandi <qwea6935@gmail.com>
2024-11-21 19:02:12 +0800

    fix: Update login page layout to use full height for better responsiveness

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -42,7 +42,7 @@ function LoginContent() {
     }
 
     return (
-        <div className="flex min-h-screen items-center justify-center">
+        <div className="flex min-h-full items-center justify-center">
             <div className="center rounded-lg bg-gray-100 px-20 py-10 font-bold text-black shadow-md">
                 <h1 className="mb-4 text-center text-2xl">登入 CommonGround</h1>
                 <div className="flex justify-center">
