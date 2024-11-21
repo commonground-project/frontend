a01c0731b94b520092bce33e1980f6c946a445f1
yukicoder0509 <bendoris0509@gmail.com>
2024-11-21 16:03:30 +0800

    feat: creat onboarding page

---
+++ src/app/onboard/page.tsx
@@ -0,0 +1,9 @@
+import OnboardingCard from "@/components/Onboarding/OnboardingCard";
+
+export default function OnboardingPage() {
+    return (
+        <main className="flex flex-col items-center pt-[76px]">
+            <OnboardingCard />
+        </main>
+    );
+}
