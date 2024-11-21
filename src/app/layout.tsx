1482df4f3e5a69a7bf7ae3774fd33d5f6db4e23b
Mrbandi <qwea6935@gmail.com>
2024-11-21 19:11:09 +0800

    refactor: Clean up layout and page components by removing unused imports and restructuring the layout for improved readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import type { Metadata } from "next";
-import Providers from "@/components/Providers";
-import localFont from "next/font/local";
-=======
 import { MantineProvider } from "@mantine/core";
 import Header from "../components/AppShell/Header";
->>>>>>> 944830c991c7227eebc1c48c11f360982b350967
 import "@mantine/core/styles.css";
 import "./globals.css";
 
@@ -16,12 +10,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-<<<<<<< HEAD
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <Providers>{children}</Providers>
-=======
             <body className="fixed inset-0 bg-neutral-200 pt-14 antialiased">
                 <MantineProvider>
                     <Header />
@@ -29,7 +17,6 @@ export default function RootLayout({
                         {children}
                     </div>
                 </MantineProvider>
->>>>>>> 944830c991c7227eebc1c48c11f360982b350967
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import { Suspense } from "react";
-import DashboardPage from "@/app/dashboard/page";
-=======
 import { mockEmptyIssue, mockIssue, mockUser } from "@/mock/conversationMock";
 import HomePageCard from "../components/HomePage/HomePageCard";
 import type { Metadata } from "next";
@@ -10,19 +6,12 @@ export const metadata: Metadata = {
     title: "CommonGround",
     description: "A platform for people to discuss and share their opinions.",
 };
->>>>>>> 944830c991c7227eebc1c48c11f360982b350967
 
 export default function Page() {
     const issues = [mockIssue, mockEmptyIssue];
     const user = mockUser;
 
     return (
-<<<<<<< HEAD
-        <div className="flex min-h-screen items-center justify-center">
-            <Suspense fallback={<div>Loading...</div>}>
-                <DashboardPage />
-            </Suspense>
-=======
         <div className="flex min-h-screen flex-col bg-neutral-200">
             <main className="flex flex-grow flex-col items-center p-8">
                 <h1 className="w-full max-w-3xl pb-3 text-2xl font-semibold text-neutral-900">
@@ -30,7 +19,6 @@ export default function Page() {
                 </h1>
                 <HomePageCard issues={issues} />
             </main>
->>>>>>> 944830c991c7227eebc1c48c11f360982b350967
         </div>
     );
 }
