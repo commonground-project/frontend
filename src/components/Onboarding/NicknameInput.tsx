a2fdb7c790c113d2b35e1bb98a8b69925b5c47fb
yukicoder0509 <bendoris0509@gmail.com>
2024-11-28 15:01:40 +0800

    refactor: using mantine useForm hook for form value management

--- src/components/Onboarding/NicknameInput.tsx
+++ src/components/Onboarding/NicknameInput.tsx
@@ -1,33 +1,37 @@
-import { Input } from "@mantine/core";
+import { TextInput } from "@mantine/core";
+import { UseFormReturnType } from "@mantine/form";
 
 type NicknameInputProps = {
-    nickname: string;
-    setNickname: (nickname: string) => void;
+    form: UseFormReturnType<
+        {
+            username: string;
+            nickname: string;
+        },
+        (values: { username: string; nickname: string }) => {
+            username: string;
+            nickname: string;
+        }
+    >;
+    inputValueName: string;
 };
 
 export default function NicknameInput({
-    nickname,
-    setNickname,
+    form,
+    inputValueName,
 }: NicknameInputProps) {
     return (
-        <Input.Wrapper
-            required
+        <TextInput
             label="暱稱"
             description="我們該如何稱呼您？"
+            required
+            {...form.getInputProps(inputValueName)}
+            key={form.key(inputValueName)}
             classNames={{
                 root: "w-full max-w-[430px] pb-[30px]",
                 label: "pb-1 text-[16px] font-semibold text-neutral-900",
-                description: "pb-2 text-[14px] font-normal text-[#868E96]",
+                description: `pb-2 text-[14px] font-normal text-[#868E96]"`,
+                input: "bg-transparent px-4 py-[6px] text-[16px] font-normal",
             }}
-        >
-            <Input
-                required
-                value={nickname}
-                onChange={(e) => setNickname(e.currentTarget.value)}
-                classNames={{
-                    input: "bg-transparent px-4 py-[6px] text-[16px] font-normal",
-                }}
-            />
-        </Input.Wrapper>
+        />
     );
 }
