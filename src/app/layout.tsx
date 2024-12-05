a02233a9b3ccf563da198474c14dd895d3dfee23
yukicoder0509 <bendoris0509@gmail.com>
2024-12-05 15:13:28 +0800

    feat: enable adding and removing facts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Providers from "./providers";
+import Header from "../components/AppShell/Header";
 import "@mantine/core/styles.css";
 import "./globals.css";
 import Providers from "./providers";
@@ -12,11 +14,13 @@ export default function RootLayout({
         <html lang="en">
             <body className="fixed inset-0 bg-neutral-200 pt-14 antialiased">
                 <Providers>
-                    <Header />
-                    <div className="scrollbar-gutter-stable-both-edges h-[calc(100vh-56px)] overflow-y-auto">
-                        {/* 56px for header */}
-                        {children}
-                    </div>
+                    <Providers>
+                        <Header />
+                        <div className="scrollbar-gutter-stable-both-edges h-[calc(100vh-56px)] overflow-y-auto">
+                            {/* 56px for header */}
+                            {children}
+                        </div>
+                    </Providers>
                 </Providers>
             </body>
         </html>
--- src/components/AuthorViewpoint/FactListCard.tsx
+++ src/components/AuthorViewpoint/FactListCard.tsx
@@ -1,20 +1,54 @@
 "use client";
 import { Fact } from "@/types/conversations.types";
+import { useDebouncedValue } from "@mantine/hooks";
 import EditViewpointFact from "@/components/AuthorViewpoint/EditViewpointFact";
 import { MagnifyingGlassIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { Select, Button } from "@mantine/core";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useMutation } from "@tanstack/react-query"; // eslint-disable-line
+import { allFacts } from "@/mock/conversationMock";
 
-type FactListCardProps = {
-    facts: Fact[];
-};
+export default function FactListCard() {
+    const [searchData, setSearchData] = useState<Fact[]>(allFacts); // eslint-disable-line
+    const [selectedFacts, setSelectedFacts] = useState<Fact[]>([]);
+    const [selectedFactId, setSelectedFactId] = useState<string>("");
+    const [searchValue, setSearchValue] = useState<string>(""); // eslint-disable-line
+    const debouncedSearchValue = useDebouncedValue(searchValue, 500); // eslint-disable-line
 
-export default function FactListCard({ facts }: FactListCardProps) {
-    const [searchData] = useState<string[]>([
-        "This development could disrupt the EV market",
-        "Google.com",
-        "CommonGround",
-    ]);
+    // const mutation = useMutation({
+    //     mutationFn: () =>
+    //         new Promise((resolve) => {
+    //             setTimeout(() => {
+    //                 resolve(console.log("Add Fact Success"));
+    //             }, 500);
+    //         }).then(() => setSearchData(allFacts)),
+    // });
+
+    // useEffect(() => {
+    //     mutation.mutate();
+    // }, [debouncedSearchValue]);
+
+    useEffect(() => {
+        const selectedFact: Fact | undefined = searchData.find(
+            (fact) => String(fact.id) == selectedFactId,
+        );
+        if (selectedFact) {
+            setSelectedFacts([...selectedFacts, selectedFact]);
+        }
+    }, [selectedFactId, setSelectedFactId]);
+
+    const removeFact = (factId: string) => {
+        const newSelectedFacts = selectedFacts.filter(
+            (fact) => String(fact.id) !== factId,
+        );
+        setSelectedFacts(newSelectedFacts);
+
+        console.log(
+            "factlist",
+            newSelectedFacts.map((fact) => fact.id),
+        );
+    };
+
     return (
         <div className="h-full rounded-lg bg-neutral-100 px-7 py-4">
             <h1 className="mb-1 text-lg font-semibold text-neutral-700">
@@ -25,24 +59,36 @@ export default function FactListCard({ facts }: FactListCardProps) {
                 <Select
                     variant="unstyled"
                     searchable
-                    data={searchData}
+                    value={searchValue}
+                    onChange={(selectedFactId) => {
+                        setSelectedFactId(selectedFactId ? selectedFactId : "");
+                        console.log("User selected id:", selectedFactId);
+                    }}
+                    data={searchData.map((fact) => ({
+                        value: String(fact.id),
+                        label: fact.title,
+                    }))}
                     checkIconPosition="right"
                     radius={0}
                     w="100%"
+                    limit={10}
                     classNames={{
                         input: "ml-2 bg-transparent text-lg font-normal text-neutral-500 focus-within:outline-b-2 focus-within:border-b-emerald-500 focus-within:outline-none",
                     }}
                     placeholder="搜尋 CommonGround"
+                    nothingFoundMessage="自行新增事實"
                 />
             </div>
             <div className="h-[calc(100vh-265px)] overflow-auto">
                 {/* 265px = 56px(header) + 69px(margin-top between header and this div) + 32px(padding-bottom of main)
                 + 92px(FactListCard title and search box) + 16px(FactListCard padding-bottom)*/}
                 <div className="flex flex-col gap-3 pl-7 pr-4">
-                    {facts.map((fact) => (
-                        <div key={fact.id}>
-                            <EditViewpointFact fact={fact} />
-                        </div>
+                    {selectedFacts.map((fact) => (
+                        <EditViewpointFact
+                            key={fact.id}
+                            fact={fact}
+                            removeFact={removeFact}
+                        />
                     ))}
                 </div>
 
