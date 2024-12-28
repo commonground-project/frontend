a25398d9c29da25a134cffa54d7b3ee3b81cc3ad
yukicoder0509 <bendoris0509@gmail.com>
2024-12-28 21:09:39 +0800

    feat: able to fetch data for issue

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,26 +1,37 @@
 import AddViewPointBar from "@/components/Conversation/ViewPoints/AddViewPointBar";
 import IssueCard from "@/components/Conversation/Issues/IssueCard";
 import ViewPointList from "@/components/Conversation/ViewPoints/ViewPointList";
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Issue } from "@/types/conversations.types";
 
 type IssueViewProps = {
     params: Promise<{ id: string }>;
 };
 
-// type MetadataProps = {
-//     params: Promise<{ id: string }>;
-// };
+type MetadataProps = {
+    params: Promise<{ id: string }>;
+};
 
-// export async function generateMetadata({
-//     params,
-// }: MetadataProps): Promise<Metadata> {
-//     return {
-//         title: `CommonGround - ${issue.title}`,
-//         keywords: "social-issues, viewpoints, rational-discussion",
-//         description: issue.description,
-//     };
-// }
+export async function generateMetadata({
+    params,
+}: MetadataProps): Promise<Metadata> {
+    const issueId = (await params).id;
+    const issue: Issue = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/issue/${issueId}`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.TMP_JWT_TOKEN}`,
+            },
+        },
+    ).then((res) => res.json());
+    return {
+        title: `CommonGround - ${issue.title}`,
+        keywords: "social-issues, viewpoints, rational-discussion",
+        description: issue.description,
+    };
+}
 
 export default async function IssueView({ params }: IssueViewProps) {
     const issueId = (await params).id;
