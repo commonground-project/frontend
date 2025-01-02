13909f96a0e36b65eb7cdd269bde78ed35ba2c46
yukicoder0509 <bendoris0509@gmail.com>
2025-01-02 23:10:26 +0800

    fix: annotate getIssue return type

--- src/lib/requests/issues/getIssue.ts
+++ src/lib/requests/issues/getIssue.ts
@@ -1,9 +1,14 @@
+import { Issue } from "@/types/conversations.types";
+
 type getIssueProps = {
     issueId: string;
     auth_token: string;
 };
 
-export const getIssue = async ({ issueId, auth_token }: getIssueProps) => {
+export const getIssue = async ({
+    issueId,
+    auth_token,
+}: getIssueProps): Promise<Issue> => {
     return await fetch(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/issue/${issueId}`,
         {
