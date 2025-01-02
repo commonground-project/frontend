db1737b22835f74605d8dc50927cf593c55fed22
burnedinthesky <40905726+burnedinthesky@users.noreply.github.com>
2025-01-02 16:44:06 +0800

    fix: Add authorization in fetching issue facts

--- src/lib/requests/issues/getIssueFacts.ts
+++ src/lib/requests/issues/getIssueFacts.ts
@@ -13,9 +13,16 @@ export type PaginatedIssueFactsByIdResponse = {
 export const getPaginatedIssueFactsById = async (
     issueid: string,
     pageParam: number,
+    auth_token: string,
 ): Promise<PaginatedIssueFactsByIdResponse> => {
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/issue/${issueid}/facts?page=${pageParam}&size=10`,
+        {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${auth_token}`,
+            },
+        },
     );
     return res.json();
 };
