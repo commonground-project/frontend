39420ad6e1959fc8879802a175b59365bf5ce794
yukicoder0509 <bendoris0509@gmail.com>
2025-01-26 14:12:48 +0800

    feat: split paragraph

--- src/lib/utils/preprocessReferenceContent.ts
+++ src/lib/utils/preprocessReferenceContent.ts
@@ -5,7 +5,7 @@ export type preprocessReferenceContentParams = {
 export type preprocessedContent = {
     type: "content" | "reference";
     text: string;
-}[][];
+}[][]; // first index is paragraph, second index is the content/reference in the paragraph
 
 export function preprocessReferenceContent({
     content,
@@ -13,38 +13,55 @@ export function preprocessReferenceContent({
     const regex = /\[([^\]]+)\]\(([^\)]+)\)/g;
     const parsedContents: preprocessedContent = [];
 
-    content.split("\n").map((paragraph) => {
-        let lastIndex = 0;
-        let match;
-        const result: { type: "content" | "reference"; text: string }[] = [];
+    let lastIndex = 0;
+    let match;
+    const result: { type: "content" | "reference"; text: string }[] = [];
 
-        while ((match = regex.exec(paragraph)) !== null) {
-            // Push normal text before the reference
-            if (lastIndex < match.index) {
-                result.push({
-                    type: "content",
-                    text: paragraph.slice(lastIndex, match.index),
-                });
-            }
-            let referenceText = match[1];
-            match[2].split(",").map((num) => {
-                // Push the reference text
-                referenceText = referenceText + `[${Number(num) + 1}]`;
-            });
-            result.push({ type: "reference", text: referenceText });
-
-            // Update the lastIndex to the end of the current match
-            lastIndex = regex.lastIndex;
-        }
-
-        // Push remaining text after the last reference
-        if (lastIndex < paragraph.length) {
+    while ((match = regex.exec(content)) !== null) {
+        // Push normal text before the reference
+        if (lastIndex < match.index) {
             result.push({
                 type: "content",
-                text: paragraph.slice(lastIndex),
+                text: content.slice(lastIndex, match.index),
             });
         }
-        parsedContents.push(result);
+        let referenceText = match[1];
+        match[2].split(",").map((num) => {
+            // Push the reference text
+            referenceText = referenceText + `[${Number(num) + 1}]`;
+        });
+        result.push({ type: "reference", text: referenceText });
+
+        // Update the lastIndex to the end of the current match
+        lastIndex = regex.lastIndex;
+    }
+
+    // Push remaining text after the last reference
+    if (lastIndex < content.length) {
+        result.push({
+            type: "content",
+            text: content.slice(lastIndex),
+        });
+    }
+
+    // Check if some text includes new line
+    // If so, split the text and push them separately to different paragraphs
+    parsedContents.push([]);
+    result.forEach((contentObject) => {
+        const splitText = contentObject.text.split("\n");
+        if (splitText.length > 1) {
+            splitText.forEach((text, index) => {
+                parsedContents[parsedContents.length - 1].push({
+                    type: contentObject.type,
+                    text,
+                });
+                if (index !== splitText.length - 1) {
+                    parsedContents.push([]);
+                }
+            });
+        } else {
+            parsedContents[parsedContents.length - 1].push(contentObject);
+        }
     });
 
     return parsedContents;
