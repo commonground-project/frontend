d19a726e62fac74bbcce31a3f78fd18efa1b07cf
yukicoder0509 <bendoris0509@gmail.com>
2025-02-16 22:44:22 +0800

    fix: move initiallization of mutation observer into useEffect

--- src/components/ReferenceMarker/ReferenceMarkerProvider.tsx
+++ src/components/ReferenceMarker/ReferenceMarkerProvider.tsx
@@ -41,75 +41,74 @@ export default function ReferenceMarkerProvider({
     const inputRef = useRef<HTMLDivElement>(null);
 
     // Setup observer on the input area
-    // Mutation observer to detect when a <span> is deleted
-    const observer = new MutationObserver((mutations) => {
-        // Dismiss the manipulation of placeholder
-        if (
-            mutations.length === 1 &&
-            ((mutations[0].addedNodes[0] as HTMLElement)?.id ===
-                "placeholder" ||
-                (mutations[0].removedNodes[0] as HTMLElement)?.id ===
-                    "placeholder")
-        )
-            return;
-        // Get the current selection (cursor position)
-        const selection = window.getSelection();
-        if (!selection || selection.rangeCount === 0) return;
-        const range = selection.getRangeAt(0);
-
-        // Set a countmap, if both start and end markers are removed,
-        // then the reference is fully removed, no need to reinsert
-        const countMap = new Map<string, Node[]>(); // id -> [start, counter, end]
-
-        mutations.forEach((mutation) => {
-            // Handle only the removal of nodes
-            if (mutation.removedNodes.length === 0) return;
-
-            // Detect the manual removal of a reference marker or reference counter
-            mutation.removedNodes.forEach((node) => {
-                if (
-                    node.nodeName === "SPAN" &&
-                    ((node as HTMLElement).classList.contains(
-                        "reference-marker",
-                    ) ||
-                        (node as HTMLElement).classList.contains(
-                            "reference-counter",
-                        ))
-                ) {
-                    const id = (node as HTMLElement).id;
-
-                    if (!countMap.has(id)) countMap.set(id, []);
-                    const cloneNode = node.cloneNode(true);
-                    countMap.get(id)?.push(cloneNode);
-                }
-            });
-        });
-
-        console.log(countMap);
-        countMap.forEach((nodes) => {
-            // If both start and end markers are removed, then the reference is fully removed
-            if (nodes.length === 3) return;
-            // If not all of the markers is removed, reinsert the whole reference
-            nodes.forEach((node) => {
-                const newSpan = node.cloneNode(true);
-                range.insertNode(newSpan);
-                range.setStartAfter(newSpan);
-                range.setEndAfter(newSpan);
-            });
-        });
-
-        selection.removeAllRanges();
-        selection.addRange(range);
-    });
-
     useEffect(() => {
         if (inputRef?.current === null) return;
+        // Mutation observer to detect when a <span> is deleted
+        const observer = new MutationObserver((mutations) => {
+            // Dismiss the manipulation of placeholder
+            if (
+                mutations.length === 1 &&
+                ((mutations[0].addedNodes[0] as HTMLElement)?.id ===
+                    "placeholder" ||
+                    (mutations[0].removedNodes[0] as HTMLElement)?.id ===
+                        "placeholder")
+            )
+                return;
+            // Get the current selection (cursor position)
+            const selection = window.getSelection();
+            if (!selection || selection.rangeCount === 0) return;
+            const range = selection.getRangeAt(0);
+
+            // Set a countmap, if both start and end markers are removed,
+            // then the reference is fully removed, no need to reinsert
+            const countMap = new Map<string, Node[]>(); // id -> [start, counter, end]
+
+            mutations.forEach((mutation) => {
+                // Handle only the removal of nodes
+                if (mutation.removedNodes.length === 0) return;
+
+                // Detect the manual removal of a reference marker or reference counter
+                mutation.removedNodes.forEach((node) => {
+                    if (
+                        node.nodeName === "SPAN" &&
+                        ((node as HTMLElement).classList.contains(
+                            "reference-marker",
+                        ) ||
+                            (node as HTMLElement).classList.contains(
+                                "reference-counter",
+                            ))
+                    ) {
+                        const id = (node as HTMLElement).id;
+
+                        if (!countMap.has(id)) countMap.set(id, []);
+                        const cloneNode = node.cloneNode(true);
+                        countMap.get(id)?.push(cloneNode);
+                    }
+                });
+            });
+
+            console.log(countMap);
+            countMap.forEach((nodes) => {
+                // If both start and end markers are removed, then the reference is fully removed
+                if (nodes.length === 3) return;
+                // If not all of the markers is removed, reinsert the whole reference
+                nodes.forEach((node) => {
+                    const newSpan = node.cloneNode(true);
+                    range.insertNode(newSpan);
+                    range.setStartAfter(newSpan);
+                    range.setEndAfter(newSpan);
+                });
+            });
+
+            selection.removeAllRanges();
+            selection.addRange(range);
+        });
         observer.observe(inputRef.current, {
             childList: true,
             subtree: true,
         });
         return () => observer.disconnect();
-    }, [inputRef, observer]);
+    }, [inputRef]);
 
     return (
         <ReferenceMarkerContext.Provider
