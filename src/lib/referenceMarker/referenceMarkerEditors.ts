c0e18e08682de371be77fa5eda7095d2484fb3b0
yukicoder0509 <bendoris0509@gmail.com>
2025-02-17 15:08:31 +0800

    feat: add comment for reference editor functions

--- src/lib/referenceMarker/referenceMarkerEditors.ts
+++ src/lib/referenceMarker/referenceMarkerEditors.ts
@@ -1,3 +1,7 @@
+// Define the funciton to do the dom operations
+// to add, remove, and update reference markers, reference counters, and highlights
+
+// Add highlight to a range of text
 function highlightTextInRange(range: Range) {
     const walker = document.createTreeWalker(
         range.commonAncestorContainer,
@@ -28,6 +32,7 @@ function highlightTextInRange(range: Range) {
     }
 }
 
+// Remove highlight from a range of text
 function dehighlightTextInRange(range: Range) {
     const walker = document.createTreeWalker(
         range.commonAncestorContainer,
@@ -59,6 +64,7 @@ type encapsuleReferenceMarkerParams = {
     referenceMarkerId: string;
 };
 
+// Generate a reference marker element to mark the start or end of a reference
 function generateReferenceMarker({
     id,
     type,
@@ -75,6 +81,7 @@ function generateReferenceMarker({
     return ReferenceMarkerElement;
 }
 
+// Encapsule (surround) a range of text with a reference marker
 export function encapsuleReferenceMarker({
     range,
     referencedIndexes,
@@ -112,6 +119,7 @@ export function encapsuleReferenceMarker({
     updateReferenceCounter({ referenceMarkerId, referencedIndexes });
 }
 
+// Update the reference counter with the new referenced indexes
 type updateReferenceMarkerParams = {
     referenceMarkerId: string;
     referencedIndexes: number[];
@@ -159,6 +167,7 @@ export function updateReferenceCounter({
     );
 }
 
+// Decapsule (remove) a reference marker from the text
 type decapsuleReferenceMarkerParams = {
     referenceMarkerId: string;
 };
