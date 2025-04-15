fea516210bb615e521605f712c723f495dd38f79
yukicoder0509 <bendoris0509@gmail.com>
2025-04-15 14:19:32 +0800

    feat: create error boundary fall back component

---
+++ src/components/AppShell/ErrorFallBack.tsx
@@ -0,0 +1,18 @@
+import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+export default function ErrorFallBack() {
+    return (
+        <div className="flex h-full w-full flex-col items-center justify-center bg-red-200">
+            <ExclamationTriangleIcon className="size-32 text-red-500" />
+            <div className="text-4xl font-bold text-red-500">Error</div>
+            <div className="font-medium text-red-500">請嘗試重新整理頁面</div>
+            <div className="font-medium text-red-500">
+                若錯誤持續發生，請
+                <Link href="mailto:contact@commonground.tw" className="inline">
+                    聯繫我們
+                </Link>
+            </div>
+        </div>
+    );
+}
