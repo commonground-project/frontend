e5aae063d2ddd4e5f19373503024cfe630116bed
yukicoder0509 <bendoris0509@gmail.com>
2025-05-05 19:23:29 +0800

    feat: add book mark or bell icon on HomePageCard base on the following status

--- src/components/HomePage/HomePageCard.tsx
+++ src/components/HomePage/HomePageCard.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import type { Issue } from "@/types/conversations.types";
 import Link from "next/link";
+import {
+    BellIcon,
+    BookmarkIcon,
+    ChatBubbleOvalLeftIcon,
+    BellIcon as __,
+} from "@heroicons/react/24/outline";
 import { forwardRef } from "react";
+import { ActionIcon } from "@mantine/core";
 
 type HomePageCardProps = {
     issue: Issue;
@@ -9,13 +18,28 @@ type HomePageCardProps = {
 const HomePageCard = forwardRef<HTMLAnchorElement, HomePageCardProps>(
     ({ issue }, ref) => {
         return (
-            <Link href={`/issues/${issue.id}`} ref={ref}>
+            <Link href={`/issues/${issue.id}`} ref={ref} className="space-y-2">
                 <h1 className="text-lg font-semibold duration-300 group-hover:text-emerald-500">
                     {issue.title}
                 </h1>
-                <p className="mt-1 whitespace-pre-wrap text-base font-normal">
+                <p className="whitespace-pre-wrap text-base font-normal">
                     {issue.description.slice(0, 100) + "..."}
                 </p>
+                <div className="flex justify-start gap-2">
+                    <div className="flex">
+                        <ChatBubbleOvalLeftIcon className="size-6 text-neutral-700" />
+                        <p className="ml-1.5 text-base text-neutral-700">
+                            {issue.viewpointCount}
+                        </p>
+                    </div>
+                    <ActionIcon variant="transparent">
+                        {issue.userFollow && issue.userFollow.follow ? (
+                            <BookmarkIcon className="size-6 text-neutral-700" />
+                        ) : (
+                            <BellIcon className="size-6 text-neutral-700" />
+                        )}
+                    </ActionIcon>
+                </div>
             </Link>
         );
     },
